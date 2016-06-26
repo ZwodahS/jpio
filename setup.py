8e0d80653dd3a49c649cabf0a1914738f17559bb
Eric Ng <ericnjf@gmail.com>
2016-06-26 04:39:40 -0400

    chore(pypi) update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name = "jpio",
     packages = find_packages(),
-    version = "0.1.0",
+    version = "0.2.0",
     description = "Json Python I/O",
     author = "Eric Ng",
     author_email = "ericnjf@gmail.com",
     url = "https://github.com/zwodahs/jpio",
     keywords = ["json", "commandline"],
-    classifiers = [],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
     entry_points = {
         "console_scripts": [
             "jpio=jpio.run_time:main",
